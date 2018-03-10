c7a172a8abd6361b8d83cdfe38c1b898ebe10168
logonrm <logonrm@fiap.com.br>
2018-03-10 14:00:30 -0300

    Inclusão da url da imagem.

--- src/main/kotlin/br/com/tatianefsantos/carroapi/entity/Carro.kt
+++ src/main/kotlin/br/com/tatianefsantos/carroapi/entity/Carro.kt
@@ -8,6 +8,7 @@ data class Carro(@Id var id: String?,
                  var marca: String,
                  var modelo: String,
                  var ano: Int,
-                 var placa: String) {
+                 var placa: String,
+                 var urlImagem: String?) {
 //    constructor() : this(id: "", marca: "", modelo: "", ano: "", placa: "")
 }
